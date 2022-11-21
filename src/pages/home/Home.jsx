dfe082f66208c0acd997a97917aa3da05b19b24e
rudalson <rudalson@gmail.com>
2022-11-22 08:25:13 +0900

    💄 added: MUI table

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
+import './home.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
-import './home.scss';
 import Widget from '../../components/widget/Widget';
 import Featured from '../../components/featured/Featured';
 import Chart from '../../components/chart/Chart';
+import Table from '../../components/table/Table';
 
 const Home = () => {
   return (
@@ -20,6 +21,11 @@ const Home = () => {
         <div className='charts'>
           <Featured />
           <Chart title='Last 6 Months (Revenue)' aspect={2 / 1} />
+        </div>
+
+        <div className='listContainer'>
+          <div className='listTitle'>Latest Transactions</div>
+          <Table />
         </div>
       </div>
     </div>
